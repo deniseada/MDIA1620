f468b0be00c8fe07b96ed4fa47ce41fd675ad156
Denise Aquino <daquino10@my.bcit.ca>
2024-09-20 15:33:55 -0700

    create stable

---
+++ stable.js
@@ -0,0 +1,10 @@
+let horseName = "matcha";
+console.log(horseName);
+horseName = "vice ganda"
+console.log(horseName);
+
+let horseAge = 123456;
+console.log(horseAge);
+
+let isHorseInside = true;
+console.log(isHorseInside);
