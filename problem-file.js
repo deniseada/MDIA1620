541aeabe2a66fbe22868f33ed203dbeb9eca4f07
Denise <daquino10@my.bcit.ca>
2024-09-26 21:01:08 -0700

    Update problem-file.js

--- problem-file.js
+++ problem-file.js
@@ -6,10 +6,13 @@ The quick brown fox jumps over the lazy spahgetti.
 
 The quick brown fox jumps over the lazy lasanga.
 
-The quick brown fox doesnt jump over the lazy dog. it just stands there and does nothing
+The quick brown fox doesnt jump over the lazy dog. it just stands there and BARKS
 
 let's also add out new terminal commands we learned!
 
 open [arg] - open a file or directory of our choice
 
 you can copy the path to your current location in the directory by using the contextual menu and "get info" on macos.
+
+Hey there, we're about to created conflict, but not with this line. This line is just to show you that you can add multiple things to a file and most of the time it will be okay.
+  
