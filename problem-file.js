425515f48d005818a236cbb96a5365bcba884a39
Denise Aquino <daquino10@my.bcit.ca>
2024-09-26 20:58:04 -0700

    update problem file

--- problem-file.js
+++ problem-file.js
@@ -1,4 +1,15 @@
 The quick brown fox jumps over the lazy DocumentFragment.
+
 The quick brown fox jumps over the lazy dog.
+
 The quick brown fox jumps over the lazy spahgetti.
+
 The quick brown fox jumps over the lazy lasanga.
+
+The quick brown fox doesnt jump over the lazy dog. it just stands there and does nothing
+
+let's also add out new terminal commands we learned!
+
+open [arg] - open a file or directory of our choice
+
+you can copy the path to your current location in the directory by using the contextual menu and "get info" on macos.
